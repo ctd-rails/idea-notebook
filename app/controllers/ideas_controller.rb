2bd5cd49afd28c89928ea353aa4dc721bf00931e
Arjun Venkataswamy <avenkat2@gmail.com>
2013-08-01 10:28:28 -0500

    adds new and create actions

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -6,4 +6,25 @@ class IdeasController < ApplicationController
       format.json { render json: @ideas}
     end
   end
+
+  def new
+    @idea = Idea.new
+    respond_to do |format|
+      format.html { render 'new' }
+    end
+  end
+
+  def create
+    Idea.create(idea_params)
+
+    respond_to do |format|
+      format.html { redirect_to ideas_url }
+    end
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:name, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 IdeaNotebook::Application.routes.draw do
 
   # Create
-
+  get "/ideas/new", { :controller => 'ideas', :action => 'new', :as => 'new_idea'}
+  post "/ideas", { :controller => 'ideas', :action => 'create', :as => 'ideas' }
 
   # Read
-  get "/ideas", { :controller => 'ideas', :action => 'index', :as => 'ideas' }
+  get "/ideas", { :controller => 'ideas', :action => 'index' }
 
   # Update
 
